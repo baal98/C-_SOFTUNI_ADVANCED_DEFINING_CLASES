4b0cdd967ecc4fb2246f455b33a349413147b8bb
Dimo Todorov <baal98@gmail.com>
2022-08-06 22:16:34 +0300

    Add files via upload

---
+++ Car Constructors/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace CarManufacturer
+{
+    class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumtion) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumtion;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+    }
+}
