93cec008ee33389ac069034d3f6a23933f3a9578
Dimo Todorov <baal98@gmail.com>
2022-08-06 22:15:55 +0300

    Add files via upload

---
+++ Car/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    internal class Class1
+    {
+    }
+    class Car
+    {
+        public Car()
+        {
+
+        }
+        private string _name;
+        private string _model;
+        private int _year;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
