4ea5f0edb315c537171576519b8c3d0cc9427485
Dimo Todorov <baal98@gmail.com>
2022-08-07 10:59:07 +0300

    Add files via upload

---
+++ Car Extension/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace CarManufacturer
+{
+    class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            var consumtion = distance * (FuelConsumption / 100);
+            if (consumtion <= FuelQuantity)
+            {
+                FuelQuantity -= consumtion;
+            }
+            else
+            {
+                System.Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}";
+        }
+    }
+}
