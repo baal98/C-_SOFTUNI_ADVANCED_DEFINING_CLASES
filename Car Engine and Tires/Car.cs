5b3103e99d5c7dc5036d0ca785354543c599a83e
Dimo Todorov <baal98@gmail.com>
2022-08-07 10:58:18 +0300

    Add files via upload

---
+++ Car Engine and Tires/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace CarManufacturer
+{
+    class Car
+    {
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumtion, Engine engine, Tire[] tires)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumtion;
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+
+
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+    }
+    public class Tire
+    {
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+}
